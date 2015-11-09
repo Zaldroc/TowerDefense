7b29e7bcedc1eaa2662bb04bb102319d63821a2e
kimeikato <iwan.kornilow001@stud.fh-dortmund.de>
2015-11-09 20:25:15 +0100

    added PathBlockEnum to PathBlock

--- GameStateManagementSample/GameObjects/PathBlock.cs
+++ GameStateManagementSample/GameObjects/PathBlock.cs
@@ -7,11 +7,43 @@ using System.Text;
 
 namespace GameStateManagementSample.GameObjects
 {
+    public enum PathBlockEnum
+    {
+        NORMAL,SPAWN,GOAL
+    }
+
     class PathBlock:GameObject
     {
-        public PathBlock(Vector2 p, Texture2D t,float s):base(p,t,s)
+        private PathBlockEnum type;
+        public PathBlock(Vector2 p, Texture2D t,float s,PathBlockEnum type):base(p,t,s)
         {
+            this.type = type;
+        }
 
+        public PathBlockEnum GetType()
+        {
+            return this.type;
+        }
+
+        public bool IsNormal()
+        {
+            if (this.type == PathBlockEnum.NORMAL)
+                return true;
+            return false;
+        }
+
+        public bool IsGoal()
+        {
+            if (this.type == PathBlockEnum.GOAL)
+                return true;
+            return false;
+        }
+
+        public bool IsSpawn()
+        {
+            if (this.type == PathBlockEnum.SPAWN)
+                return true;
+            return false;
         }
     }
 }
