4cc47d8166a9e48977288dfbb720d3d82c645547
kimeikato <iwan.kornilow001@stud.fh-dortmund.de>
2015-11-24 19:20:26 +0100

    created method IsConnectedToOtherPathBlock

--- GameStateManagementSample/GameObjects/Level.cs
+++ GameStateManagementSample/GameObjects/Level.cs
@@ -23,8 +23,8 @@ namespace GameStateManagementSample.GameObjects
         {
             Vector2 levelSize = new Vector2(32, 18);
 
-            grid = new int[Convert.ToInt16(levelSize.X), Convert.ToInt16(levelSize.Y)];
-            map = new int[Convert.ToInt16(levelSize.X)*100, Convert.ToInt16(levelSize.Y)*100];
+            grid = new int[(int)levelSize.X, (int)levelSize.Y];
+            map = new int[(int)levelSize.X*100, (int)levelSize.Y*100];
 
             for(int i=0;i<levelSize.X;i++)
                 for(int j=0;j<levelSize.Y;j++)
@@ -62,13 +62,40 @@ namespace GameStateManagementSample.GameObjects
                     break;
             }
 
-            grid[Convert.ToInt32(pathBlock.GetPosition().X) / 100, Convert.ToInt32(pathBlock.GetPosition().Y) / 100] = value;
-            path.Add(pathBlock);
+            if (IsConnectedToOtherPathBlock(pathBlock))
+            {
+                grid[(int)pathBlock.GetPosition().X / 100, (int) pathBlock.GetPosition().Y / 100] = value;
+                path.Add(pathBlock);
+            }
         }
 
         private bool IsConnectedToOtherPathBlock(PathBlock pathBlock)
         {
             Vector2 pos = new Vector2(Convert.ToInt32(pathBlock.GetPosition().X) / 100, Convert.ToInt32(pathBlock.GetPosition().Y) / 100);
+
+            if (pathBlock.IsNormal())
+            {
+                Vector2 north = new Vector2(0,-1) + pos;
+                Vector2 south = new Vector2(0, 1) + pos;
+                Vector2 east = new Vector2(1, 0) + pos;
+                Vector2 west = new Vector2(-1, 0) + pos;
+
+                int n = grid[((int)north.X), ((int)north.Y)];
+                int s = grid[((int)south.X), ((int)south.Y)];
+                int e = grid[((int)east.X), ((int)east.Y)];
+                int w = grid[((int)west.X), ((int)west.Y)];
+
+                if((n > 0 && n < 4)||
+                    (s > 0 && s < 4)||
+                    (e > 0 && e < 4)||
+                    (w > 0 && w < 4))
+                {
+                    return true;
+                }
+
+            }
+            else
+                return true;
 
             return false;
         }
